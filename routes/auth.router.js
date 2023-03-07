4215978262624036587bf5b0ee58d000f3214d74
Marcelo Leda Filho <83438543+Marceloleda@users.noreply.github.com>
2023-03-07 17:45:47 -0400

    Update auth.router.js

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,10 +1,4 @@
 import express from 'express';
-import { signUp, signIn } from '../controllers/auth.controller.js';
-import {validateSignUp, validateSignIn} from '../middlewares/validateAuth.middleware.js';
 
-const router = express.Router();
-
-router.post('/signup', validateSignUp, signUp);
-router.post('/signin', validateSignIn, signIn);
 
 export default router;
