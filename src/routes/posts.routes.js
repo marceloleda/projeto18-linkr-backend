bb1e1829ce8773d732a968fee8c3428639ca1544
Igor Holanda <oigorholanda@gmail.com>
2023-03-09 01:52:45 -0300

    feat: posts routers and folders refactor

---
+++ src/routes/posts.routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const postsRouter = Router();
+
+postsRouter.get("/timeline", );
+postsRouter.post("/timeline", );
+postsRouter.put("/timeline", );
+postsRouter.delete("/timeline/:id", );
+
+export default postsRouter;
