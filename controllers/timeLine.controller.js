a22e67a255d4cecec6defecfc1757c0923933600
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-03-10 13:49:49 -0400

    timeline metadata

--- controllers/timeLine.controller.js
+++ controllers/timeLine.controller.js
@@ -1,12 +1,39 @@
-import { db } from "../database/database.connection";
-export async function timeLine(){
-    try{
-       const {rows: posts} = db.query(`SELECT * FROM posts;`)
+import { db } from "../database/database.connection.js";
+import urlMetadata from 'url-metadata';
 
-       res.send(posts[0])
+
+
+export async function getPosts(req, res){
+    
+    try{
+       const metadata = await urlMetadata('https://medium.com/@pshrmn/');
+       const { image, description, title, url } = metadata;
+       console.log({image, description, title, url});
+
+       const {rows: posts} = await db.query(`
+       SELECT * 
+       FROM posts
+       ORDER BY created_at DESC
+       LIMIT 20
+       ;`)
+
+       res.send(posts)
     }
     catch(error){
         console.log(error.message)
         return res.sendStatus(500);
     }
+}
+export async function createPost(req, res){
+    const {userId, link, description} = req.body;
+    try{
+        await db.query(`INSERT INTO posts (user_id, link, description) VALUES ($1, $2, $3);`,
+        [userId, link, description])
+      
+        res.sendStatus(201)
+     }
+     catch(error){
+         console.log(error.message)
+         return res.sendStatus(500);
+     }
 }
