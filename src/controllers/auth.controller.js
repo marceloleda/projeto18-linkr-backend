0107b8d7474eaa0b9c3f53b3c0fcc899e9a67324
Hudson Peres <hudsonluizperes@poli.ufrj.br>
2023-03-10 20:47:49 -0300

    feat: debug try 1

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,24 +1,31 @@
 import { db } from "../database/database.connection.js";
 import bcrypt from 'bcrypt';
 import dotenv from "dotenv";
+import jwt from 'jsonwebtoken';
+
 dotenv.config();
 
+const SECRET_KEY = process.env.SECRET_KEY;
+
 export async function getUserByEmail(email) {
-    return (await db.query(`SELECT * FROM users WHERE email = $1`, [email]))
-  }
-  
+  const result = await db.query(`SELECT * FROM users WHERE email = $1`, [email]);
+  return result.rows[0];
+}
+
 export async function createUser(user) {
-    await db.query(`INSERT INTO users VALUES(DEFAULT, $1, $2, $3, $4)`, [
-        user.email,
-        user.password,
-        user.username,
-        user.picture_url
-    ]);
+  console.log('Creating user:', user);
+  await db.query(`INSERT INTO users (email, password, username, picture_url) VALUES ($1, $2, $3, $4)`, [
+    user.email,
+    user.password,
+    user.username,
+    user.picture_url
+  ]);
 }
 
 export async function signIn(req, res) {
   try {
     const user = res.locals.user;
+    const token = jwt.sign({ user }, SECRET_KEY, { expiresIn: '1h' });
     res.status(200).send({ token, user });
   } catch (err) {
     res.status(500).send(err.message);
@@ -27,22 +34,24 @@ export async function signIn(req, res) {
 
 export async function signUp(req, res) {
   try {
-    const body = req.body;
+    const { email, password, username, picture_url } = req.body;
+    console.log("test")
 
-    const hasUserWithEmail = await getUserByEmail(body.email);
+    const hasUserWithEmail = (await getUserByEmail(email)).length > 0;
 
-    if (hasUserWithEmail) return res.sendStatus(409);
+    if (hasUserWithEmail) {
+      return res.status(409).send({ message: 'E-mail already registered.' });
+    }
+    const passwordHash = await bcrypt.hash(password, 10);
 
-    const passwordHash = bcrypt.hashSync(body.password, 10);
+    await createUser({ email, password: passwordHash, username, picture_url });
 
-    await createUser({ ...body, password: passwordHash });
-
-    res.sendStatus(201);
+    res.status(201).send({ message: 'User created successfully.' });
   } catch (err) {
     console.log(err);
     res.status(500).send({
       success: false,
-      message: "Erro ao cadastrar",
+      message: "Error while registering",
       exception: err,
     });
   }
--- src/controllers/jwt.controller.js
+++ src/controllers/jwt.controller.js
@@ -1,41 +1,43 @@
-import jsonwebtoken from "jsonwebtoken";
-import { PRIVATE_KEY, user, tokenValited } from "../models/auth.schema";
+// import jwt from 'jsonwebtoken';
+// import { SECRET_KEY, user, tokenValited } from '../models/auth.schema';
+// import express from 'express';
 
-api.get('/', (_, res) => res.status(200).json({
-    message: 'this is a PUBLIC router...'
-}));
+// const api = express();
 
-api.get('/signin', (req, res) => {
-    const [,hash] = req.headers.authorization?.split(' ') || [' ', ' '];
-    const [email, password] = Buffer.from(hash, 'base64').toString().split(':');
+// api.get('/', (_, res) => res.status(200).json({
+//     message: 'this is a PUBLIC router...'
+// }));
+
+// api.get('/signin', (req, res) => {
+//     const [,hash] = req.headers.authorization?.split(' ') || [' ', ' '];
+//     const [email, password] = Buffer.from(hash, 'base64').toString().split(':');
     
-    try {
-        const correctPassword = email === 'aaaa@a.com' && password === '123456';
-        if (!correctPassword) return response.status(401).sen('Password or E-mail incorrect!');
+//     try {
+//         const correctPassword = email === 'aaaa@a.com' && password === '123456';
+//         if (!correctPassword) return res.status(401).send('Password or E-mail incorrect!');
         
-        const token = jsonwebtoken.sign(
-            { user: JSON.stringify(user) },
-            PRIVATE_KEY,
-            { expiresIn: '60m' }
-        );
+//         const token = jwt.sign(
+//             { user: JSON.stringify(user) },
+//             SECRET_KEY,
+//             { expiresIn: '60m' }
+//         );
 
-        return res.status(200).json({ data: { user, token} });
-    } catch(err) {
-        console.log(err);
-        return res.send(err);
-    }
-});
+//         return res.status(200).json({ data: { user, token} });
+//     } catch(err) {
+//         console.log(err);
+//         return res.send(err);
+//     }
+// });
 
-api.use('*', tokenValited);
+// api.use('*', tokenValited);
 
-api.get('/private', (req, res) => {
-    const { user } = req.headers
-    const currentUser = JSON.parse(user);
-    return res.status(200).json({
-        message: 'This is a PRIVATE router...',
-        data: {
-            userLogged: currentUser
-        }
-    })
-});
-
+// api.get('/SECRET', (req, res) => {
+//     const { user } = req.headers
+//     const currentUser = JSON.parse(user);
+//     return res.status(200).json({
+//         message: 'This is a SECRET router...',
+//         data: {
+//             userLogged: currentUser
+//         }
+//     })
+// });
