cf39a9a93791eb24ee3157a7959307e8cd870c66
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-03-10 18:45:40 -0400

    timeline metadata

--- controllers/timeLine.controller.js
+++ controllers/timeLine.controller.js
@@ -1,23 +1,32 @@
 import { db } from "../database/database.connection.js";
 import urlMetadata from 'url-metadata';
 
-
-
 export async function getPosts(req, res){
-    
+    const user_id = '1';
     try{
-       const metadata = await urlMetadata('https://medium.com/@pshrmn/');
-       const { image, description, title, url } = metadata;
-       console.log({image, description, title, url});
-
-       const {rows: posts} = await db.query(`
-       SELECT * 
-       FROM posts
-       ORDER BY created_at DESC
-       LIMIT 20
-       ;`)
-
-       res.send(posts)
+        const {rows: posts} = await db.query(`
+        SELECT 
+        posts.id, 
+        posts.link, 
+        posts.description, 
+        posts.created_at, 
+        json_build_object(
+            'title', metadata_posts.title, 
+            'description', metadata_posts.description, 
+            'image', metadata_posts.image, 
+            'url', metadata_posts.url
+        ) as metadata,
+        json_build_object(
+            'username', users.username,
+            'picture_url', users.picture_url
+        ) as user_info
+        FROM posts
+        LEFT JOIN metadata_posts ON metadata_posts.post_id = posts.id
+        LEFT JOIN users ON users.id = posts.user_id
+        ORDER BY posts.created_at DESC
+        LIMIT 20;`)
+       
+       res.status(200).send(posts)
     }
     catch(error){
         console.log(error.message)
@@ -25,11 +34,32 @@ export async function getPosts(req, res){
     }
 }
 export async function createPost(req, res){
-    const {userId, link, description} = req.body;
+    const {link, description} = req.body;
+    const userId ='1';
     try{
         await db.query(`INSERT INTO posts (user_id, link, description) VALUES ($1, $2, $3);`,
         [userId, link, description])
-      
+
+        const {rows: posts} = await db.query(`
+        SELECT posts.id 
+        FROM posts
+        ORDER BY posts.id
+        DESC
+        LIMIT 1
+        ;`)
+   
+        const metadata = await urlMetadata(`${link}`);
+
+        const title = metadata.title;
+        const descriptio = metadata.description;
+        const image = metadata.image;
+        const url = metadata.url;
+
+        await db.query(
+            'INSERT INTO metadata_posts (post_id, image, description, title, url) VALUES ($1, $2, $3, $4, $5);',
+            [posts[0].id, image, descriptio, title, url]
+          );
+        
         res.sendStatus(201)
      }
      catch(error){
