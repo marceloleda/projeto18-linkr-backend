99b9f9a827da2d9ab86039277f58e5d80813c5aa
Igor Holanda <oigorholanda@gmail.com>
2023-03-13 19:03:37 -0300

    fix: api routes and login

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { createPost, getPosts } from '../controllers/timeLine.controller.js';
+import { authentication } from '../middlewares/jwt.middleware.js';
 
 const postsRouter = Router();
 
-postsRouter.get("/timeline", );
-postsRouter.post("/timeline", );
+postsRouter.get("/timeline", getPosts);
+postsRouter.post("/timeline", createPost);
 postsRouter.put("/timeline", );
 postsRouter.delete("/timeline/:id", );
 
